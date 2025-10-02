00508d7205040c7b9bd31da72fdfdeb468bb18de
kenken1199 <nakamori.k@gmail.com>
2025-10-02 10:03:02 +0900

    add home

--- app/page.js
+++ app/page.js
@@ -1,9 +1,36 @@
-const Home = () => {
+import Link from "next/link";
+import Image from "next/image";
+import connectDB from "./utils/database";
+import { ItemModel } from "./utils/schemaModels";
+
+const itemReadAll = async () => {
+  await connectDB();
+  const allItems = await ItemModel.find();
+  return allItems;
+};
+
+const ReadAllItems = async () => {
+  const allItems = await itemReadAll();
   return (
     <div>
-      <h1>こんちには</h1>
+      {allItems?.map((item) => (
+        <Link href="" key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <div>
+            <h2>&yen;{item.price}</h2>
+            <h3>{item.title}</h3>
+            <p>{item.description.substring(0, 80)}...</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
 
-export default Home;
+export default ReadAllItems;

eabc382054be5a52ef223cfb998a2d5dd5c554d3
kenken1199 <nakamori.k@gmail.com>
2025-10-02 17:19:47 +0900

    add

--- app/item/update/[id]/page.js
+++ app/item/update/[id]/page.js
@@ -5,7 +5,37 @@ const UpdateItem = async (context) => {
   return (
     <div>
       <h1>アイテム編集</h1>
-      {singleItem.title}
+      <form>
+        <input
+          defaultValue={singleItem.title}
+          type="text"
+          name="title"
+          placeholder="アイテム名"
+          required
+        />
+        <input
+          defaultValue={singleItem.price}
+          type="text"
+          name="price"
+          placeholder="価格"
+          required
+        />
+        <input
+          defaultValue={singleItem.image}
+          type="text"
+          name="image"
+          placeholder="画像"
+          required
+        />
+        <textarea
+          defaultValue={singleItem.description}
+          name="description"
+          rows={15}
+          placeholder="商品説明"
+          required
+        ></textarea>
+        <button>作成</button>
+      </form>
     </div>
   );
 };
